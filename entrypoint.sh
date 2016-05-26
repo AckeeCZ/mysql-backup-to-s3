2bee72c3282698e577bbb10cde1b89f4347d4f11
Josef Gattermayer <josef.gattermayer@ackee.cz>
2016-05-26 18:48:40 +0200

    fixed variable verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 # verify variables
-if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o "$MYSQL_HOST" -o "$MYSQL_PORT"]; then
+if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o -z "$MYSQL_HOST" -o -z "$MYSQL_PORT"]; then
 	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, MYSQL_URL, MYSQL_PORT. No backups, no fun.'
 	exit 1
 fi
@@ -19,7 +19,7 @@ s3cmd ls "s3://$S3_URL" > /dev/null
    echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
 
 # add a cron job
-echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mysqldump -u root -p'"'"'$MYSQL_ROOT_PASSWORD'"'"' --all-databases --single-transaction --force -h "$MYSQL_HOST" -P "$MYSQL_PORT" | gzip > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
+echo "$CRON_SCHEDULE root rm -rf /tmp/dump && mysqldump -u root -p'$MYSQL_ROOT_PASSWORD' --all-databases --single-transaction --force -h "$MYSQL_HOST" -P "$MYSQL_PORT" | gzip > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ && rm -rf /tmp/dump" >> /etc/crontab
 crontab /etc/crontab
 
 exec "$@"
